ac9f736e632906c86b741f746eaf34e02a24e0fd
JorPan <jbpanasewicz@gmail.com>
2020-11-20 19:04:30 -0700

    commit tree 3d0c46ee604cded851d0f61395c7f79e93701c8a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--)
+}};
+
+
+function writeCards(arr, event) {
+    let dumb = []
+    for (let i=0; i<arr.length; i++) {
+        dumb.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return dumb
+}
